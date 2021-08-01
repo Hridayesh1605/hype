6253ebfbdac796394c87dcbe9efb5bca4b275171
YASH-2711 <57184467+YASH-2711@users.noreply.github.com>
2021-08-01 19:04:53 +0530

    last message

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -5,16 +5,22 @@ import { database } from '../../misc/firebase';
 import EditableInput from '../EditableInput';
 import AvatarUploadBtn from './AvatarUploadBtn';
 import ProviderBlock from './ProviderBlock';
+import { getUserUpdates } from '../../misc/helpers';
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
+
   const onSave = async newData => {
-    const userNicknameRef = database
-      .ref(`/profile/${profile.uid}`)
-      .child('name');
     try {
-      await userNicknameRef.set(newData);
-      Alert.success('Nickname Has been changed', 4000);
+      const updates = await getUserUpdates(
+        profile.uid,
+        'name',
+        newData,
+        database
+      );
+      await database.ref().update(updates);
+
+      Alert.success('Nickname Has been updated', 4000);
     } catch (err) {
       Alert.error(err.message, 4000);
     }
